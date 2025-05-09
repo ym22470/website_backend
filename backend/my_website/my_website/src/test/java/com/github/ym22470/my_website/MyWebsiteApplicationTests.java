714cc78b6b73362b3cffc6d5306213ae56f5f764
Chongsheng Wang <wcs1809325658@gmail.com>
2025-05-09 16:04:48 +0100

    initial commit

---
+++ backend/my_website/my_website/src/test/java/com/github/ym22470/my_website/MyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ym22470.my_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/src/main/java/com/github/ym22470/my_website/MyWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.ym22470.my_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebsiteApplication.class, args);
+	}
+
+}
